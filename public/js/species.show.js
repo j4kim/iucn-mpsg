5858f5fe8ebd218c3b9d62ec7de784f55558f664
joaquim <joaquim.perez@he-arc.ch>
2017-09-21 14:41:17 +0200

    code pour photoswipe

--- public/js/species.show.js
+++ public/js/species.show.js
@@ -1,3 +1,26 @@
-/**
- * Created by Boh on 20.09.2017.
- */
+var pswpElement = document.querySelectorAll('.pswp')[0];
+
+// build items array
+var items = [
+    {
+        src: '/uploads/images/Abies_1.jpg',
+        w: 3870,
+        h: 2570
+    },
+    {
+        src: '/uploads/images/Abies_2.jpg',
+        w: 1200,
+        h: 900
+    }
+];
+
+// define options (if needed)
+var options = {
+    // optionName: 'option value'
+    // for example:
+    index: 0 // start at first slide
+};
+
+// Initializes and opens PhotoSwipe
+var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options);
+gallery.init();
