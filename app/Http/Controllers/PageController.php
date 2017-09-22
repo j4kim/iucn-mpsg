6c77e69cb5fb7b85d6f83c89b2e51e7de2390e30
joaquim <joaquim.perez@he-arc.ch>
2017-09-22 14:29:38 +0200

    pas besoin d'être co pour voir le livre

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -10,7 +10,7 @@ class PageController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth', ['except' => ['index','show']]);
+        $this->middleware('auth', ['except' => ['index','show', 'book']]);
     }
 
     /**
