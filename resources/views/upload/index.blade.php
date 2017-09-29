1562dded9b6b28ab6476c35399f74bfd75c8d718
joaquim <joaquim.perez@he-arc.ch>
2017-09-29 23:28:32 +0200

    la vue de mise en ligne de fichiers est upload.index

--- resources/views/upload/index.blade.php
+++ resources/views/upload/index.blade.php
@@ -3,20 +3,21 @@
 @section('title',"Upload")
 
 @section('content')
+    <p></p>
     <form class="form-horizontal" action="{{ route('upload.store')}}" method="POST" enctype="multipart/form-data">
         {{ csrf_field() }}
 
         <div class="form-group">
             <label class="control-label col-sm-2">Files</label>
             <div class="col-sm-10">
-                <div>
-                    <label for="files-input" class="form-control btn btn-primary">Add files</label>
-                    <input type="file" id="files-input" style="display:none">
+                <div style="margin-top: 7px">
+                    <input type="file" id="files-input" name="files[]" multiple>
                 </div>
             </div>
         </div>
 
-        <button type="submit" class="btn btn-primary">Upload</button>
+        <button type="submit" class="col-sm-offset-2 btn btn-primary">Upload</button>
 
     </form>
+    <hr>
 @endsection
