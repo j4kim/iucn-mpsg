47d8361bc65fa08def393bcdc34e985eb502aded
joaquim <joaquim.perez@he-arc.ch>
2017-09-29 19:19:29 +0200

    script pour que l'image d'header couvre toujours son container

---
+++ public/js/main.js
@@ -0,0 +1,17 @@
+$(function(){
+    function aspect(elem){
+        return elem.width()/elem.height()
+    }
+
+    $(window).resize(function(){
+        var image = $(".img-header img");
+        var header = $(".img-header");
+        if(aspect(header) > aspect(image)){
+            // cas desktop
+            image.css({"width": "100%", "height":"initial"});
+        }else{
+            // cas mobile
+            image.css({"height": "100%", "width": "initial", "max-width": "initial"});
+        }
+    })
+});
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -14,6 +14,8 @@
     <!-- Bootstrap Latest compiled and minified JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
+    <script src="{{ asset('js/main.js') }}"></script>
+
     @yield('head')
 
     <link rel="stylesheet" href="{{ asset('css/main.css') }}">
